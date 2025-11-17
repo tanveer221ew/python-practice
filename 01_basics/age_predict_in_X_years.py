5e0e146101220939bb0513c107956e944a083a0b
Tanveer Ahmed <tanveerchandio432@gmail.com>
2025-11-17 21:09:11 +0500

    First Exercise

---
+++ 01_basics/age_predict_in_X_years.py
@@ -0,0 +1,6 @@
+#1.	Take your name and age, and predict your age in 5 years.
+name = input("Enter your name:")
+age = int(input("Enter your age:"))
+userAge = int(input("Enter you desired age:"))
+# print("Your age in 5 years will be", age+5)
+print(f"Your age is {userAge} years will be", age+userAge)
