1f6455143fa204730a29004aefd2b48a4a709c48
Tanveer Ahmed <tanveerchandio432@gmail.com>
2025-11-18 20:07:32 +0500

    New exercise added

---
+++ 01_basics/math_op_on_two_numbers.py
@@ -0,0 +1,10 @@
+#5.	Perform all math operations (+, -, *, /, %, //, **) on two numbers
+num_one = int(input("Enter the first number:"))
+num_two = int(input("Enter the second number:"))
+
+additon = num_one+num_two
+substraction = num_one-num_two
+multiplication = num_one*num_two
+division = num_one/num_two
+
+print(f"Addition is {additon}\nSubstraction is {substraction}\nMultiplication is {multiplication}\nDivision is {division}")
