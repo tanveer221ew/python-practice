4e4f87e1c74bf248642842a2ad3971bcf7ca4f0e
Tanveer Ahmed <tanveerchandio432@gmail.com>
2025-11-18 19:27:58 +0500

    Added new exercise

---
+++ 01_basics/reverse_string.py
@@ -0,0 +1,6 @@
+#Reverse a string manually without using slicing
+str = str(input("Enter your string:"))
+reversed_str = "".join(reversed(str))
+print(f"Original string is {str}, reversed string is {reversed_str}")
+
+
