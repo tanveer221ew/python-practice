70e5200fb8406c42c9f7c6cec5e897aefc8b3f00
Tanveer Ahmed <tanveerchandio432@gmail.com>
2025-11-19 21:09:10 +0500

    New exercise added

---
+++ 01_basics/str_modify.py
@@ -0,0 +1,5 @@
+# 9.	Convert string to uppercase, lowercase, and title case.
+str = "This is a string"
+print(f"Upper case: {str.upper()}")
+print(f"Lowercase: {str.lower()}")
+print(f"Title case: {str.title()}")
