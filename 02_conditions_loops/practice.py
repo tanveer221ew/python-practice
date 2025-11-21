d44ca16a9d881cf2c0d4da6f00e995d3ee05d09a
Tanveer Ahmed <tanveerchandio432@gmail.com>
2025-11-21 23:16:24 +0500

    New exercise added

--- 02_conditions_loops/practice.py
+++
@@ -1,8 +0,0 @@
-# counter = 10
-# while counter <= 10:
-#     print(counter)
-#     counter += 1
-counter = 10
-while counter >= 10:   # LOGIC ERROR: always true
-    print(counter)
-    counter += 1
---
+++ practice.py
@@ -0,0 +1,7 @@
+# 5.	Find largest of three numbers.
+# numbers = [10, 11, 111]
+# largest = 11
+# for num in numbers:
+#     if num > largest:
+#         largest = num
+# print(largest)
