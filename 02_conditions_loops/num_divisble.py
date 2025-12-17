c4fcd572d592385fdd215fe99488bee8dcae7d5e
Tanveer Ahmed <tanveerchandio432@gmail.com>
2025-12-17 21:35:18 +0500

    Check divisibilty by 3 and 5

---
+++ 02_conditions_loops/num_divisble.py
@@ -0,0 +1,6 @@
+# Check if a number is divisible by 3 and 5.
+num = int(input("Enter a number:"))
+if num % 3 == 0 and num % 5 == 0:
+    print("It is divisble by 3 and 5")
+else:
+    print("Not divisble")
